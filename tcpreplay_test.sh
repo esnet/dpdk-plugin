007b6c51bb8aafb4ebace1cc16163a79189322dc
Vlad Grigorescu <vlad@es.net>
2021-08-20 09:44:19 -0500

    Add support for side-by-side perfs

--- tcpreplay_test.sh
+++ tcpreplay_test.sh
@@ -33,18 +33,31 @@ EOF
     ssh $HOST -f "./$C1FILENAME"
 }
 
+RUN_ID=$(uuidgen)
+echo "Started run with ID $RUN_ID"
+
+ssh zeek-test1.es.net "rm tmp.*"
 ssh zeek-test2.es.net "rm tmp.*"
 
-ZEEK_PID=$(run_cmd zeek-test2.es.net "pkill -9 zeek; pkill -9 perf; /usr/local/zeek/bin/zeek -i dpdk::ens3f1" /tmp/zeek.out zeek)
-echo "Started Zeek with PID $ZEEK_PID"
+ZEEK_DPDK_PID=$(run_cmd zeek-test2.es.net "pkill -9 zeek; pkill -9 perf; /usr/local/zeek/bin/zeek -i dpdk::ens3f1" /tmp/zeek.out zeek)
+echo "Started DPDK Zeek with PID $ZEEK_DPDK_PID"
 
-PERF_PID=$(run_cmd zeek-test2.es.net "perf record -g -F199 --call-graph dwarf -p $ZEEK_PID -o - | perf script | c++filt" /tmp/perf_zeek-test2.out perf)
-echo "Started perf with PID $PERF_PID"
+PERF_DPDK_PID=$(run_cmd zeek-test2.es.net "perf record -g -F199 --call-graph dwarf -p $ZEEK_DPDK_PID -o - | perf script | c++filt" /tmp/perf_zeek-test2.out perf)
+echo "Started DPDK perf with PID $PERF_DPDK_PID"
+
+ZEEK_AF_PID=$(run_cmd zeek-test1.es.net "pkill -9 zeek; pkill -9 perf; /usr/local/zeek/bin/zeek -i af_packet::ens3f0 " /tmp/zeek.out zeek)
+echo "Started AF_Packet Zeek with PID $ZEEK_AF_PID"
+
+PERF_AF_PID=$(run_cmd zeek-test1.es.net "perf record -g -F199 --call-graph dwarf -p $ZEEK_AF_PID -o - | perf script | c++filt" /tmp/perf_zeek-test1.out perf)
+echo "Started AF_Packet perf with PID $PERF_AF_PID"
 
 echo "Starting tcpreplay"
 ssh zeek-west1.es.net -f 'sudo tcpreplay -i enp216s0f1 -Kt /usr/local/esnet-security/pcaps/west_dc.pcap'
 
-ssh zeek-test2.es.net -f "sudo kill $ZEEK_PID; sleep 3; sudo kill -INT $PERF_PID"
+ssh zeek-test2.es.net -f "sudo kill $ZEEK_DPDK_PID; sleep 3; sudo kill -INT $PERF_DPDK_PID"
+ssh zeek-test1.es.net -f "sudo kill $ZEEK_AF_PID; sleep 3; sudo kill -INT $PERF_AF_PID"
 
-scp zeek-test2.es.net:/tmp/perf_zeek-test2.out .
+scp zeek-test1.es.net:/tmp/perf_zeek-test1.out perf_zeek-test1_$RUN_ID.out
+scp zeek-test2.es.net:/tmp/perf_zeek-test2.out perf_zeek-test2_$RUN_ID.out
 
+echo "Finished run with ID $RUN_ID"
